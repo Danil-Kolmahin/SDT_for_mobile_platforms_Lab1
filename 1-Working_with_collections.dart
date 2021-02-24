453913f1b53ab659267a797ecc6a97103704b3b0
Kolmagin Danil <dkolmagin@gmail.com>
2021-02-24 11:22:53 +0200

    Changes in reading time #1

--- 1-Working_with_collections.dart
+++ 1-Working_with_collections.dart
@@ -1,34 +1,34 @@
-var list = [1, 'a', [], () => {}];
-var set = {1, 1, 'a', [], () => {}};
-var map = {
-  'a': 0,
-  true: [],
-  () => {}: 1.0001
-};
-
-dynamic list1 = [];
-var list2 = [];
-List list3 = []; // == List<dynamic>
-List<int> list4 = [1];
-List<List<int>> list5 = [
-  [1],
-  [1, 2]
-];
-
-final list6 = []; // cannot be reassigned
-var list7 = const []; // compile-time constant
-const list8 = []; // == final list8 = const []; contains final
-
 main() {
+  var list = [1, 'a', [], () => {}];
+  var set = {1, 1, 'a', [], () => {}};
+  var map = {
+    'a': 0,
+    true: [],
+    () => {}: 1.0001
+  };
+
+  // print(list);
+  // print(set);
+  // print(map);
+
+  dynamic list1 = [];
+  var list2 = [];
+  List list3 = []; // == List<dynamic>
+  List<int> list4 = [1];
+  List<List<int>> list5 = [
+    [1],
+    [1, 2]
+  ];
+
   // list1 = {};
   // list2 = {};
+
+  final list6 = []; // cannot be reassigned
+  var list7 = const []; // compile-time constant
+  const list8 = []; // == final list8 = const []; contains final
 
   // list6 = [];
   // list7.add(5); // cannot add to an unmodifiable list
   // list8 = [];
   // list8.add(5);
-
-  print(list);
-  print(set);
-  print(map);
 }
